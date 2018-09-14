c5556e4fa74f077c011e4522468f1d22c6b0a6df
qiuqingwen <qqw@uplink.net.cn>
2018-09-14 15:55:20 +0800

    .

--- WebMobile/Controllers/PersonalController.cs
+++ WebMobile/Controllers/PersonalController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMobile.Models.Personal;
 
 namespace WebMobile.Controllers
 {
-    public class PersonalController : Controller
+    public class PersonalController : OwnBaseController
     {
         public ActionResult Index()
         {
             ViewBag.ShowHeader = false;
 
-            return View();
+            IndexViewModel model = new IndexViewModel();
+
+            model.LoadData(this.CurrentUserId);
+
+            return View(model);
         }
     }
 }
---
+++ WebMobile/Models/Personal/IndexViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMobile.Models.Personal
+{
+    public class IndexViewModel : OwnBaseViewModel
+    {
+        private string _headImgUrl = null;
+        private string _nickname = null;
+        private string _availableBalance = "0.00";
+
+        public string HeadImgUrl
+        {
+            get
+            {
+                return _headImgUrl;
+            }
+            set
+            {
+                _headImgUrl = value;
+            }
+        }
+        public string Nickname
+        {
+            get
+            {
+                return _nickname;
+            }
+            set
+            {
+                _nickname = value;
+            }
+        }
+
+        public string AvailableBalance
+        {
+            get
+            {
+                return _availableBalance;
+            }
+            set
+            {
+                _availableBalance = value;
+            }
+        }
+
+
+
+        public void LoadData(string userId)
+        {
+            var wxUserInfo = CurrentDb.WxUserInfo.Where(m => m.UserId == userId).FirstOrDefault();
+            if (wxUserInfo != null)
+            {
+                _nickname = wxUserInfo.Nickname;
+                _headImgUrl = wxUserInfo.HeadImgUrl;
+
+                var fund = CurrentDb.Fund.Where(m => m.UserId == userId).FirstOrDefault();
+
+                if (fund != null)
+                {
+                    _availableBalance = fund.AvailableBalance.ToF2Price();
+                }
+            }
+        }
+    }
+}
