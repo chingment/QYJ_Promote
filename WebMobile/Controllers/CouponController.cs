b8cbac497bb9c5d232ddf3d94018a5682899fcad
qiuqingwen <qqw@uplink.net.cn>
2018-09-15 12:17:28 +0800

    .

--- WebMobile/Controllers/CouponController.cs
+++ WebMobile/Controllers/CouponController.cs
@@ -1,17 +1,78 @@
-﻿using System;
+﻿using Lumos;
+using Lumos.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMobile.Models.Coupon;
 
 namespace WebMobile.Controllers
 {
-    public class CouponController : Controller
+    public class CouponController : OwnBaseController
     {
- 
+
         public ActionResult My()
         {
             return View();
+        }
+
+        [HttpPost]
+        public CustomJsonResult GetMy(SearchCondition model)
+        {
+            var query = (from o in CurrentDb.PromoteUserCoupon
+                         where
+                         o.UserId == this.CurrentUserId &&
+                         o.IsBuy == true
+                         select new { o.Id, o.WxCouponId, o.WxCouponDecryptCode, o.IsGet, o.IsConsume, o.CreateTime });
+
+            int total = query.Count();
+
+            int pageSize = 10;
+
+            query = query.OrderByDescending(r => r.CreateTime).Skip(pageSize * (model.PageIndex)).Take(pageSize);
+
+            var list = query.ToList();
+
+            List<MyCouponModel> oList = new List<MyCouponModel>();
+
+            foreach (var item in list)
+            {
+                var myTran = new MyCouponModel();
+                myTran.Name = "代金券";
+                myTran.Discounttip = "";
+                myTran.Validdate = "2018-09-08~2019-09-30";
+                myTran.WxCouponId = item.WxCouponId;
+                myTran.WxCouponDecryptCode = item.WxCouponDecryptCode;
+                myTran.Amount = "7200";
+                myTran.Discounttip = "报读课程使用";
+                myTran.Description = "·具体详情到校区咨询";
+                if (!item.IsGet)
+                {
+                    myTran.Status = 1;
+                    myTran.StatusName = "待领取";
+                }
+                else
+                {
+                    if (item.IsConsume)
+                    {
+                        myTran.Status = 3;
+                        myTran.StatusName = "已核销";
+                    }
+                    else
+                    {
+                        myTran.Status = 2;
+                        myTran.StatusName = "待核销";
+                    }
+                }
+
+                oList.Add(myTran);
+            }
+
+            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = oList };
+
+            return new CustomJsonResult(ResultType.Success, ResultCode.Success, "", pageEntity);
+
         }
     }
 }
---
+++ WebMobile/Models/Coupon/MyCouponModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMobile.Models.Coupon
+{
+    public class MyCouponModel
+    {
+        public string Name { get; set; }
+
+        public string Amount { get; set; }
+
+        public string Discounttip { get; set; }
+
+        public string Validdate { get; set; }
+
+        public string Description { get; set; }
+
+        public string StatusName { get; set; }
+
+        public int Status { get; set; }
+
+        public string WxCouponId { get; set; }
+
+        public string WxCouponDecryptCode { get; set; }
+    }
+}
