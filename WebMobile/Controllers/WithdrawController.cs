b3cb32b395d413653509d5493d4022fd05dda42f
qiuqingwen <qqw@uplink.net.cn>
2018-09-16 16:51:54 +0800

    .

--- WebMobile/Controllers/WithdrawController.cs
+++ WebMobile/Controllers/WithdrawController.cs
@@ -6,23 +6,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMobile.Models.Withdraw;
 
 namespace WebMobile.Controllers
 {
     public class WithdrawController : OwnBaseController
     {
-        // GET: Withdraw
-        public ActionResult Index()
+        public ActionResult My()
         {
             return View();
         }
-
 
         [HttpPost]
         public CustomJsonResult Apply(WithdrawApplyPms model)
         {
             model.UserId = this.CurrentUserId;
             return BizFactory.Withdraw.Apply(this.CurrentUserId, model);
+        }
+
+        [HttpPost]
+        public CustomJsonResult GetMy(SearchCondition model)
+        {
+            var query = (from o in CurrentDb.Withdraw
+                         where
+                          o.UserId == this.CurrentUserId
+                         select new { o.Sn, o.ApplyTime, o.Amount, o.Status, o.AcName, o.ApplyMethod, o.AcIdNumber, o.FailureReason });
+
+            int total = query.Count();
+
+            int pageSize = 10;
+
+            query = query.OrderByDescending(r => r.ApplyTime).Skip(pageSize * (model.PageIndex)).Take(pageSize);
+
+            var list = query.ToList();
+
+            List<MyWithdrawModel> oList = new List<MyWithdrawModel>();
+
+            foreach (var item in list)
+            {
+                var myWithdraw = new MyWithdrawModel();
+                myWithdraw.Sn = item.Sn;
+                myWithdraw.Title = "提现到我的账户";
+                myWithdraw.ApplyTime = item.ApplyTime.ToString("yyyy.MM.dd HH:mm:ss");
+                myWithdraw.Amount = item.Amount.ToF2Price();
+
+                switch (item.Status)
+                {
+                    case Enumeration.WithdrawStatus.Apply:
+                    case Enumeration.WithdrawStatus.Handing:
+                        myWithdraw.Remarks = "处理中";
+                        myWithdraw.RemarksFontColor = "#000000";
+                        break;
+                    case Enumeration.WithdrawStatus.Success:
+                        myWithdraw.Remarks = "成功";
+                        myWithdraw.RemarksFontColor = "#1fc31b";
+                        break;
+                    case Enumeration.WithdrawStatus.Failure:
+                        myWithdraw.Remarks = "失败";
+                        if (!string.IsNullOrEmpty(item.FailureReason))
+                        {
+                            myWithdraw.Remarks += "，" + item.FailureReason;
+                        }
+                        myWithdraw.RemarksFontColor = "#f34754";
+                        break;
+
+                }
+
+                oList.Add(myWithdraw);
+            }
+
+            PageEntity pageEntity = new PageEntity { PageSize = pageSize, TotalRecord = total, Rows = oList };
+
+            return new CustomJsonResult(ResultType.Success, ResultCode.Success, "", pageEntity);
+
         }
     }
 }
---
+++ WebMobile/Models/Withdraw/MyWithdrawModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMobile.Models.Withdraw
+{
+    public class MyWithdrawModel
+    {
+        public string Sn { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Remarks { get; set; }
+        public string RemarksFontColor { get; set; }
+        public string ApplyTime { get; set; }
+    }
+}
