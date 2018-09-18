bf1aeb7ff12c4a9f50f08f3e8dd2daca734895ca
qiuqingwen <qqw@uplink.net.cn>
2018-09-18 12:35:06 +0800

    .

--- WebMobile/Models/Withdraw/ApplyViewModel.cs
+++ WebMobile/Models/Withdraw/ApplyViewModel.cs
@@ -9,6 +9,12 @@ namespace WebMobile.Models.Withdraw
     {
         private string _availableBalance = "0.00";
 
+        private string _acName = "";
+        private string _acIdNumber = "";
+        private string _acBank = "";
+        private string _acBankCardNumber = "";
+
+
         public string AvailableBalance
         {
             get
@@ -18,6 +24,51 @@ namespace WebMobile.Models.Withdraw
             set
             {
                 _availableBalance = value;
+            }
+        }
+
+        public string AcName
+        {
+            get
+            {
+                return _acName;
+            }
+            set
+            {
+                _acName = value;
+            }
+        }
+        public string AcIdNumber
+        {
+            get
+            {
+                return _acIdNumber;
+            }
+            set
+            {
+                _acIdNumber = value;
+            }
+        }
+        public string AcBank
+        {
+            get
+            {
+                return _acBank;
+            }
+            set
+            {
+                _acBank = value;
+            }
+        }
+        public string AcBankCardNumber
+        {
+            get
+            {
+                return _acBankCardNumber;
+            }
+            set
+            {
+                _acBankCardNumber = value;
             }
         }
 
