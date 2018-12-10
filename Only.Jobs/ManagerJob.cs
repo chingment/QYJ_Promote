ca0551b5130d328b8a79a2e6eb9bdb9437501830
qiuqingwen <chingment@126.com>
2018-12-10 10:10:26 +0800

    .

---
+++ Only.Jobs/ManagerJob.cs
@@ -0,0 +1,34 @@
+﻿using log4net;
+using Lumos.BLL.Task;
+using Quartz;
+using System;
+
+namespace Only.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class ManagerJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(ManagerJob));
+
+        public void Execute(IJobExecutionContext context)
+        {
+            string curRunId = Guid.NewGuid().ToString();
+            Version Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            _logger.InfoFormat("ManagerJob[{0}] Execute begin Ver.", Ver.ToString(), curRunId);
+            try
+            {
+                QuartzManager.JobScheduler(context.Scheduler);
+                _logger.InfoFormat("ManagerJob[{0}] Executing ...", curRunId);
+            }
+            catch (Exception ex)
+            {
+                JobExecutionException e2 = new JobExecutionException(ex);
+                e2.RefireImmediately = true;
+            }
+            finally
+            {
+                _logger.InfoFormat("ManagerJob[{0}] Execute end ", curRunId);
+            }
+        }
+    }
+}
---
+++ Only.Jobs/Program.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using System;
+using System.IO;
+using System.Reflection;
+using Topshelf;
+
+namespace Only.Jobs
+{
+    class Program
+    {
+        public static ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log.InfoFormat("程序开始");
+
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            HostFactory.Run(x =>
+            {
+                x.UseLog4Net();
+                x.RunAsLocalSystem();
+                x.Service<ServiceRunner>();
+                x.SetDescription(string.Format("{0} Ver:{1}", System.Configuration.ConfigurationManager.AppSettings.Get("ServiceName"), System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()));
+                x.SetDisplayName(System.Configuration.ConfigurationManager.AppSettings.Get("ServiceDisplayName"));
+                x.SetServiceName(System.Configuration.ConfigurationManager.AppSettings.Get("ServiceName"));
+                x.EnablePauseAndContinue();
+            });
+        }
+    }
+}
